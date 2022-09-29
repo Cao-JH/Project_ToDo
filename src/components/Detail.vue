e2c93d419f2dd8bcc8d2b8451349ea3bddb786c4
liu060608 <200021cjh@gmail.com>
2022-09-29 17:58:17 +0800

    子窗口能读到数据了

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -39,10 +39,55 @@
                 </n-collapse-item>
             </n-collapse>
             <div class="addContent">
-                <svg-icon className="addIcon" iconName="add" />
+                <svg-icon className="addIcon" iconName="add" @click="addListContent" />
             </div>
         </div>
+    </div>
+    <div class="mask" v-show="maskShow">
+        <div class="backmask" @click="closeAdd"></div>
+        <div class="addForm">
+            <div class="addHeader">新增</div>
+            <div class="addBody">
+                <div class="inputBox">
+                    项目名: <input type="text" v-model="addData.projectTitle">
+                </div>
+                <div class="textareaBox" contenteditable="true" @input="saveText($event)">
 
+                </div>
+            </div>
+            <div class="addFooter">
+                <n-popconfirm :show-icon="false" positive-text="好的" :negative-text="null">
+                    <template #trigger>
+                        <button @click="projectPost">提交</button>
+                    </template>
+                    账号不能为空
+                </n-popconfirm>
+                <button @click="closeAdd">取消</button>
+            </div>
+        </div>
+    </div>
+    <div class="mask" v-show="maskShow">
+        <div class="backmask" @click="closeAdd"></div>
+        <div class="addForm">
+            <div class="addHeader">新增</div>
+            <div class="addBody">
+                <div class="inputBox">
+                    项目名: <input type="text" v-model="addData.projectTitle">
+                </div>
+                <div class="textareaBox" contenteditable="true" @input="saveText($event)">
+
+                </div>
+            </div>
+            <div class="addFooter">
+                <n-popconfirm :show-icon="false" positive-text="好的" :negative-text="null">
+                    <template #trigger>
+                        <button @click="projectPost">提交</button>
+                    </template>
+                    账号不能为空
+                </n-popconfirm>
+                <button @click="closeAdd">取消</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -51,16 +96,25 @@ import { onBeforeMount, reactive, ref } from 'vue';
 import { NTime, NCollapse, NCollapseItem, NRadio } from 'naive-ui'
 // 引入路由获取query参数
 import { useRoute } from 'vue-router';
-import { Project } from '@/types/project'
+import { Project, ProjectContent, ListContent } from '@/types/project'
 
 // route实例
 const route = useRoute()
 
-const { ipcRenderer } = window.require("electron");
-
 // 获取数据
-const projectDetail = async (data: object) => {
-
+const projectDetail = (data: object) => {
+    // 读取本次存储，判断是否有需要的内容
+    let result = localStorage.getItem('projects')
+    if (result != null) {
+        const projectList = JSON.parse(result)
+        for (let i = 0; i < projectList.length; i++) {
+            // 找到id对应的内容，赋值
+            if (projectList[i].id == data) {
+                console.log(projectList[i]);
+                list.value = projectList[i]
+            }
+        }
+    }
 }
 
 // ceshi
@@ -68,26 +122,36 @@ const getProjectData = async () => {
 
 }
 
+// 提取query中的id
+var queryId: any
+for (let id in route.query) {
+    queryId = route.query[id]
+}
+
 onBeforeMount(() => {
-    projectDetail(route.query)
+    projectDetail(queryId)
     getProjectData()
 })
 
 // 定义数据
 const list = ref({} as Project)
+const projectContent = ref([] as ProjectContent[])
+const ListContent = ref({} as ListContent)
 
 const saveText = () => {
     // 输入内容即可触发事件，可以使用保存方法，还要加节流
     console.log(1111);
 }
 
+// 添加二级内容
+const addListContent = () => {
+}
 
 const ddd = () => {
     console.log(route.query);
     projectDetail(route.query)
 
 }
-
 </script>
 
 <style lang="less" scoped>
@@ -223,6 +287,93 @@ const ddd = () => {
         border: 1px solid #252422;
         border-radius: 25px;
         text-align: center;
+    }
+}
+
+.mask {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+
+
+    .backmask {
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.3);
+        z-index: 1;
+    }
+
+    .addForm {
+        position: absolute;
+        top: calc(50% - 120px);
+        left: calc(50% - 250px);
+        width: 500px;
+        height: 240px;
+        padding: 10px 15px;
+        background-color: #d3d3d3;
+        border-radius: 20px;
+        border: 3px solid #161a1d;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        z-index: 2;
+
+        .addHeader {
+            border-bottom: 1px solid #0b090a;
+        }
+
+        .addBody {
+
+            .inputBox {
+                margin: 12px 50px;
+                width: 375px;
+                text-align: center;
+                font-size: 16px;
+
+                input {
+                    border: 0;
+                    background-color: #d3d3d3;
+                    border-bottom: 2px solid #0b090a;
+                    width: 200px;
+                    font-size: 20px;
+                    padding: 0 5px;
+                    outline: none;
+                }
+            }
+
+            .textareaBox {
+                border: 2px solid #000;
+                border-radius: 5px;
+                padding: 0 5px;
+                margin: 0 50px;
+                outline: none;
+                max-height: 95px;
+                // 暂时使用hidden，后面修改滚轮样式
+                overflow: hidden;
+            }
+        }
+
+        .addFooter {
+            text-align: center;
+
+            button {
+                width: 50px;
+                height: 30px;
+                margin: 0 10px;
+                font-size: 14px;
+                border: 2px solid #000;
+                border-radius: 5px;
+            }
+
+            button:nth-child(1) {
+                background-color: #84a98c;
+            }
+
+            button:nth-child(2) {
+                background-color: #e56b6f;
+            }
+        }
     }
 }
 </style>
